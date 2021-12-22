cb3db748fb45d8c9e1e424195c19b416cb8e1651
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-12-22 17:16:39 -0500

    added project prompts and simple validation

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ inquirer.prompt([
     {
       type: 'input',
       name: 'firstName',
-      message: 'Enter your first name.'
+      message: 'Enter your first name.',
+      validate(answer) {
+        if(!answer) {
+          return "Please provide your first name."
+        }
+        return true
+      }
     },
     {
       type: 'input',
@@ -35,8 +41,43 @@ inquirer.prompt([
     {
       type: 'input',
       name: 'email',
-      message: 'Enter your email address.'
+      message: 'Enter your email address.',
+      // could improve by used  a regex to validate valid email https://pakstech.com/blog/inquirer-js/
+      validate(answer) {
+        if(!answer) {
+          return "Please provide an email address."
+        }
+        return true
+      }
+    },
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'Enter the name of your project.'
     }
+    ,
+    {
+      type: 'input',
+      name: 'projectImage',
+      message: 'Enter the url for a thumbnail image of the project.'
+    },
+    {
+      type: 'input',
+      name: 'projectInfo',
+      message: 'Enter a brief 1-2 sentence description of the project.'
+    },
+    {
+      type: 'input',
+      name: 'projectLink',
+      message: 'Enter a url for the project repo OR the deployed app.',
+      validate(answer) {
+        if(answer.includes(' ')) {
+          return "Please provide only one url."
+        }
+        return true
+      }
+    }
+
   ])
   .then((response) => 
     console.log(response)
